0611c793a10d225fb24fcb462352fdbc5e25c5db
lokeshbs1994 <lokeshbs619@gmail.com>
2022-04-22 20:05:57 +0530

    modified

--- bin/lyric_generator.dart
+++ bin/lyric_generator.dart
@@ -1,24 +1,24 @@
 void main() async {
   print('Ready sing');
-  await line1();
-  await line2();
+  line1();
+  line2();
   line3();
   print(await line4());
 }
 
-Future<void> line1() async {
-  return await Future.delayed(
+Future<void> line1() {
+  return Future.delayed(
       Duration(seconds: 2), () => print('Happy Birthday to You.'));
 }
 
-Future<void> line2() async {
-  return await Future.delayed(
-      Duration(seconds: 3), () => print('Happy Birthday to You..'));
+Future<void> line2() {
+  return Future.delayed(
+      Duration(seconds: 5), () => print('Happy Birthday to You..'));
 }
 
 Future<void> line3() {
   return Future.delayed(
-      Duration(seconds: 2), () => print('Happy Birthday, Happy Birthday...'));
+      Duration(seconds: 8), () => print('Happy Birthday, Happy Birthday...'));
 }
 
 Future<String> line4() async {
